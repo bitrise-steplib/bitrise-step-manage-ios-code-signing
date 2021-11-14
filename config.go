ea3efe07245a4fc03ffc8275ae947e8ebcdabb44
Mate Herber <mate.herber@bitrise.io>
2021-11-14 16:35:50 +0100

    Initial commit of migrated inputs

---
+++ config.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/bitrise-io/go-steputils/stepconf"
+	"github.com/bitrise-io/go-utils/sliceutil"
+	"github.com/bitrise-io/go-xcode/appleauth"
+	"github.com/bitrise-io/go-xcode/autocodesign"
+	"github.com/bitrise-io/go-xcode/autocodesign/certdownloader"
+)
+
+// Config holds the step inputs
+type Config struct {
+	Distribution  string `env:"distribution_method,opt[development,app-store,ad-hoc,enterprise],required"`
+	ProjectPath   string `env:"project_path,dir,required"`
+	Scheme        string `env:"scheme,required"`
+	Configuration string `env:"configuration"`
+
+	BitriseConnection string `env:"apple_service_connection,opt[api-key,apple-id],required"`
+
+	RegisterTestDevices bool `env:"register_test_devices,opt[yes,no],required"`
+	MinProfileDaysValid int  `env:"min_profile_validity,required"`
+	SignUITestTargets   bool `env:"sign_uitest_targets,opt[yes,no],required"`
+
+	CertificateURLList        string          `env:"certificate_url_list,required"`
+	CertificatePassphraseList stepconf.Secret `env:"passphrase_list,required"`
+	KeychainPath              string          `env:"keychain_path,required"`
+	KeychainPassword          stepconf.Secret `env:"keychain_password,required"`
+	BuildAPIToken             string          `env:"build_api_token"`
+	BuildURL                  string          `env:"build_url"`
+
+	VerboseLog bool `env:"verbose_log,opt[no,yes]"`
+}
+
+// DistributionType ...
+func (c Config) DistributionType() autocodesign.DistributionType {
+	return autocodesign.DistributionType(c.Distribution)
+}
+
+// ValidateCertificates validates if the number of certificate URLs matches those of passphrases
+func (c Config) ValidateCertificates() ([]string, []string, error) {
+	pfxURLs := splitAndClean(c.CertificateURLList, "|", true)
+	passphrases := splitAndClean(string(c.CertificatePassphraseList), "|", false)
+
+	if len(pfxURLs) != len(passphrases) {
+		return nil, nil, fmt.Errorf("certificates count (%d) and passphrases count (%d) should match", len(pfxURLs), len(passphrases))
+	}
+
+	return pfxURLs, passphrases, nil
+}
+
+// Certificates returns an array of p12 file URLs and passphrases
+func (c Config) Certificates() ([]certdownloader.CertificateAndPassphrase, error) {
+	pfxURLs, passphrases, err := c.ValidateCertificates()
+	if err != nil {
+		return nil, err
+	}
+
+	files := make([]certdownloader.CertificateAndPassphrase, len(pfxURLs))
+	for i, pfxURL := range pfxURLs {
+		files[i] = certdownloader.CertificateAndPassphrase{
+			URL:        pfxURL,
+			Passphrase: passphrases[i],
+		}
+	}
+
+	return files, nil
+}
+
+// SplitAndClean ...
+func splitAndClean(list string, sep string, omitEmpty bool) (items []string) {
+	return sliceutil.CleanWhitespace(strings.Split(list, sep), omitEmpty)
+}
+
+func parseAuthSources(bitriseConnection string) ([]appleauth.Source, error) {
+	switch bitriseConnection {
+	case "automatic":
+		return []appleauth.Source{
+			&appleauth.ConnectionAPIKeySource{},
+			&appleauth.InputAPIKeySource{},
+		}, nil
+	case "api_key":
+		return []appleauth.Source{
+			&appleauth.ConnectionAPIKeySource{},
+		}, nil
+	case "apple_id", "apple-id", "enterprise_with_apple_id", "enterprise-with-apple-id":
+		return []appleauth.Source{
+			&appleauth.ConnectionAppleIDFastlaneSource{},
+		}, nil
+	case "off":
+		return []appleauth.Source{
+			&appleauth.InputAPIKeySource{},
+		}, nil
+	default:
+		return nil, fmt.Errorf("invalid connection input: %s", bitriseConnection)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/bitrise-steplib/bitrise-step-ios-automatic-code-signing
+
+go 1.16
+
+require (
+	github.com/bitrise-io/go-steputils v0.0.0-20211112094629-32e127e755f4
+	github.com/bitrise-io/go-utils v0.0.0-20211110205926-af3caef627ae
+	github.com/bitrise-io/go-xcode v0.0.0-20211112092016-c326de6beaab
+	github.com/bitrise-io/pkcs12 v0.0.0-20211108084543-e52728e011c8 // indirect
+)
